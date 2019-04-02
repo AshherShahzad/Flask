8b29cfcd1290fe86e9cf85bfff8a0004cc278f6a
Ashher Shahzad <AshherShahzad@gmail.com>
2019-04-02 13:54:42 +0500

    Flask class 2 work

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 #from flask import Flask
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 # Index
 @app.route('/')
@@ -14,6 +17,17 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+#Articles
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+#Single Article
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html',article=article)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
---
+++ data.py
@@ -0,0 +1,46 @@
+def Articles():
+    articles = [
+        {
+        'id': 1,
+        'title':'Article 1',
+        'body':'Article body',
+        'author':'Asher Shahzad',
+        'created':'12-10-2018'
+        },
+        {
+        'id': 2,
+        'title':'Article 2',
+        'body':'Article body',
+        'author':'Asher Shahzad',
+        'created':'12-10-2018'
+        },
+        {
+        'id': 3,
+        'title':'Article 3',
+        'body':'Article body',
+        'author':'Asher Shahzad',
+        'created':'12-10-2018'
+        },
+        {
+        'id': 4,
+        'title':'Article 4',
+        'body':'Article body',
+        'author':'Asher Shahzad',
+        'created':'12-10-2018'
+        },
+        {
+        'id': 5,
+        'title':'Article 5',
+        'body':'Article body',
+        'author':'Asher Shahzad',
+        'created':'12-10-2018'
+        },
+        {
+        'id': 6,
+        'title':'Article 6',
+        'body':'Article body',
+        'author':'Asher Shahzad',
+        'created':'12-10-2018'
+        }
+    ]
+    return articles
